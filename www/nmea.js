a2602eca23acdfd9ab6eb517d551538a4c19eff3
Gurzixo <gurzixo@platinn.commail>
2025-10-27 16:44:15 +0000

    nmea smoother

--- www/nmea.js
+++ www/nmea.js
@@ -1,5 +1,36 @@
 var exec = require("cordova/exec");
 
+const SPEED_THRESHOLD_MPS = 0.5;
+const POSITION_ALPHA = 0.15;
+
+class NmeaSmoother {
+    constructor() {
+        this.filtered = null;
+    }
+
+    reset() {
+        this.filtered = null;
+    }
+
+    update({ latitude, longitude, altitude, speed }) {
+        if (speed >= SPEED_THRESHOLD_MPS || !this.filtered) {
+            this.filtered = { latitude, longitude, altitude };
+            return { latitude, longitude, altitude };
+        }
+
+        this.filtered = {
+            latitude: lerp(this.filtered.latitude, latitude, POSITION_ALPHA),
+            longitude: lerp(this.filtered.longitude, longitude, POSITION_ALPHA),
+            altitude: lerp(this.filtered.altitude, altitude, POSITION_ALPHA),
+        };
+        return { ...this.filtered };
+    }
+}
+
+function lerp(a, b, alpha) {
+    return a + (b - a) * alpha;
+}
+
 var nmea = {
     watch: function (success, error) {
         exec(success, error, "Nmea", "watch", []);
@@ -8,6 +39,12 @@ var nmea = {
     clearWatch: function (success, error) {
         exec(success, error, "Nmea", "clearWatch", []);
     },
+
+    createSmoother: function () {
+        return new NmeaSmoother();
+    },
+
+    NmeaSmoother,
 };
 
 module.exports = nmea;
